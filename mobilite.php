1b4dbeee4b7fea15ab9f1d5d8530f3e3d28a83ac
popschool <popschool@debian>
2017-09-25 09:37:43 +0200

    add mobility.php seb

--- mobilite.php
+++ mobilite.php
@@ -4,3 +4,20 @@ include('include/header.php');
 include('include/footer.php');
 
 ?>
+<div class="container">
+
+<a href="https://www.voyages-sncf.com/" target="_blank">
+<div class="section image opacity">
+</div></a>
+
+<a href="http://www.tadao.fr/" target="_blank"><div class="section image2 opacity">
+</div></a>
+
+
+<a href="http://www.taxi-rapide.fr/taxi/commune/lens/62300" target="_blank"><div class="section image3 opacity">
+</div></a>
+
+  <a href="https://www.idvroom.com/" target="_blank"><div class="section image4 opacity">
+
+</div></a>
+</div>
